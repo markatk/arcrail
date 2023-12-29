c91837950ab0a7d0d3b96c54907ef1749ee771a7
Markus Kerspe <web@grigull.net>
2023-12-29 12:21:27 +0100

    Add support for atmega328pb

--- Arcrail/timer.cpp
+++ Arcrail/timer.cpp
@@ -1,6 +1,6 @@
 #include "timer.h"
 
-#if defined(__AVR_ATmega168P__) || defined(__AVR_ATmega328P__)
+#if defined(__AVR_ATmega168P__) || defined(__AVR_ATmega328P__) || defined(__AVR_ATmega328PB__)
     #define TIMER_MAX 50 // timer0 is configured at 2ms, trigger should happen every 100ms
 #else
     #error Timer module is not defined for this board!
@@ -13,7 +13,7 @@ void timer_init() {
     _trigger_count = 0;
     _timer_count = 0;
 
-#if defined(__AVR_ATmega168P__) || defined(__AVR_ATmega328P__)
+#if defined(__AVR_ATmega168P__) || defined(__AVR_ATmega328P__) || defined(__AVR_ATmega328PB__)
     // Timer0 is already used for millis() - we'll just interrupt somewhere
     // in the middle and call the "Compare A" function below
     OCR0A = 0x32;
