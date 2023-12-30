a008f417fc02de1c7ff62f7a45e0a74d87c07321
Markus Kerspe <web@grigull.net>
2023-12-30 14:37:41 +0100

    Add loconet feedback decoder rev a config

---
+++ Arcrail/boards/loconet-feedback-decoder-rev-a.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Arduino.h"
+
+//===========================================================================
+//======================== Loconet ==========================================
+//===========================================================================
+
+#define USE_LOCONET
+#define PIN_LOCONET_RX 8
+#define PIN_LOCONET_TX 6
+
+//===========================================================================
+//======================== Inputs ===========================================
+//===========================================================================
+
+#define USE_INPUTS
+#define INPUT_COUNT 8
+
+#define INPUT_1 9
+#define INPUT_2 7
+#define INPUT_3 10
+#define INPUT_4 11
+#define INPUT_5 12
+#define INPUT_6 13
+#define INPUT_7 A0
+#define INPUT_8 A1
+
+// create input pin array
+// TODO: Create array in configuration.h with C preprocessor macro
+const uint8_t INPUTS[INPUT_COUNT] = {
+    INPUT_1,
+    INPUT_2,
+    INPUT_3,
+    INPUT_4,
+    INPUT_5,
+    INPUT_6,
+    INPUT_7,
+    INPUT_8};
