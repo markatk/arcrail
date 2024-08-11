d2e697384dd86c16041968f45ab331cbf4018839
Markus Kerspe <web@grigull.net>
2024-08-11 10:56:28 +0200

    Add public mode and getter functions to leds
    Add missing status led functions

--- Arcrail/src/led.h
+++ Arcrail/src/led.h
@@ -2,6 +2,11 @@
 
 #include "../configuration.h"
 
+// TODO: Rename off to static or something as off seems like the led cannot be turned on
+#define LED_MODE_OFF 0
+#define LED_MODE_BLINK 1
+#define LED_MODE_FLASH 2
+
 void led_init();
 
 void led_update();
@@ -9,15 +14,25 @@ void led_update();
 #ifdef USE_LEDS
 void led_set(uint8_t led, uint8_t value);
 
+bool led_get(uint8_t led);
+
 void led_toggle(uint8_t led);
 
 void led_blink(uint8_t led);
 
 void led_flash(uint8_t led);
 
+uint8_t led_get_mode(uint8_t led);
+
     #ifdef STATUS_LED
 void status_led_set(uint8_t value);
 
+bool status_led_get();
+
 void status_led_blink();
+
+void status_led_flash();
+
+uint8_t status_led_get_mode();
     #endif
 #endif
